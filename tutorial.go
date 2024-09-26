82d7f0cb18135ec7655de1646944e6c27d3fb355
yhavin <y.havin@gmail.com>
2024-09-26 11:11:04 -0400

    Add order completion

--- tutorial.go
+++ tutorial.go
@@ -3,22 +3,35 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
+type product struct {
+	name  string
+	price float64
 }
 
-var products = []string{"Onions", "Carrots", "Bread", "Chicken thighs", "Ribeye steak", "Salmon"}
+var products = []product{
+	{"Onions", 1.99},
+	{"Carrots", 1.99},
+	{"Bread", 3.99},
+	{"Chicken", 8.99},
+	{"Ribeye steaks", 16.99},
+	{"Salmon", 12.99},
+}
+
+type model struct {
+	cursor        int
+	selected      map[int]bool
+	orderComplete bool
+}
 
 func initialModel() model {
 	return model{
-		choices:  products,
-		selected: make(map[int]struct{}),
+		cursor:   0,
+		selected: make(map[int]bool),
 	}
 }
 
@@ -37,41 +50,55 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.cursor--
 			}
 		case "down":
-			if m.cursor < len(m.choices) - 1 {
+			if m.cursor < len(products)-1 {
 				m.cursor++
 			}
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
+		case " ":
+			m.selected[m.cursor] = !m.selected[m.cursor]
+		case "enter":
+			m.orderComplete = true
+			return m, tea.Quit
 		}
 	}
 
 	return m, nil
-} 
+}
 
 func (m model) View() string {
-	s := "Select the products you'd like to buy\n\n"
+	s := "Select the products you'd like to buy (space to select)\n\n"
 
-	for i, choice := range m.choices {
+	s += fmt.Sprintf("%-5s %-22s | %8s\n", "", "Product", "Price")
+	s += fmt.Sprintf("%-5s %-22s | %8s\n", "", strings.Repeat("-", 22), strings.Repeat("-", 8))
+
+	for i, product := range products {
 		cursor := " "
 		if m.cursor == i {
 			cursor = ">"
 		}
 
 		checked := " "
-		_, ok := m.selected[i]
-		if ok {
+		if m.selected[i] {
 			checked = "x"
 		}
 
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+		s += fmt.Sprintf("%s [%-1s] %-22s | $%7.2f\n", cursor, checked, product.name, product.price)
 	}
 
-	s += "\nPress q to quit\n"
+	s += "\nEnter to complete order, q to quit\n"
+
+	if m.orderComplete && len(m.selected) > 0 {
+		var totalPrice float64
+		var selectedItems []string
+
+		for i, selected := range m.selected {
+			if selected {
+				selectedItems = append(selectedItems, products[i].name)
+				totalPrice += products[i].price
+			}
+		}
+
+		s += fmt.Sprintf("\nBought: %s\nTotal price: $%.2f\n", strings.Join(selectedItems, ", "), totalPrice)
+	}
 
 	return s
 }
